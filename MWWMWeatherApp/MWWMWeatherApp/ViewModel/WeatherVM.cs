d8d7fd5426e561065c49f13f76089dfa98d05572
Oskar <kogutoskar123@gmail.com>
2019-04-21 14:33:25 +0200

    Added WeatherVM, and changed WeatherAPI

---
+++ MWWMWeatherApp/MWWMWeatherApp/ViewModel/WeatherVM.cs
@@ -0,0 +1,85 @@
+﻿using MWWMWeatherApp.Model;
+using System.Collections.ObjectModel;
+
+namespace MWWMWeatherApp.ViewModel {
+    /// <summary>
+    /// The main ViewModel of the application.
+    /// It is the closest one to the view.
+    /// It's purpouse is to update the view via binding.
+    /// </summary>
+    class WeatherVM {
+
+        private string query;
+        private City selectedCity;
+
+        /// <summary>
+        /// The query written by the user in a TextBox
+        /// </summary>
+        public string Query {
+            get { return query; }
+            set {
+                query = value;
+                GetCities();
+            }
+        }
+
+        // Observable collection is dynamic data collection 
+        // that provides notifications when items get 
+        // added, removed, or when the whole list is refreshed
+
+        /// <summary>
+        /// DailyForecasts for the city choosed by the user
+        /// </summary>
+        public ObservableCollection<DailyForecast> Forecasts { get; set; }
+
+        /// <summary>
+        /// Cities that need to show up when user
+        /// types in a value for the query in the TextBox
+        /// </summary>
+        public ObservableCollection<City> Cities { get; set; }
+
+        /// <summary>
+        /// City selected by the user
+        /// </summary>
+        public City SelectedCity {
+            get { return selectedCity; }
+            set {
+                selectedCity = value;
+                GetWeather();
+            }
+        }
+
+        public WeatherVM() {
+            Forecasts = new ObservableCollection<DailyForecast>();
+            Cities = new ObservableCollection<City>();
+            SelectedCity = new City();
+        }
+
+        /// <summary>
+        /// Change the data of the Cities ObservableCollection
+        /// so that the view can bind
+        /// </summary>
+        private async void GetCities() {
+            var cities = await WeatherAPI.GetAutocompleteAsync(Query);
+
+            Cities.Clear();
+            foreach (var city in cities) {
+                Cities.Add(city);
+            }
+        }
+
+        /// <summary>
+        /// Change the data of the Forecasts ObservableCollection
+        /// so that the view can bind
+        /// </summary>
+        private async void GetWeather() {
+            string key = SelectedCity.Key;
+            var weather = await WeatherAPI.GetWeatherInformationAsync(SelectedCity.Key);
+
+            Forecasts.Clear();
+            foreach (var forecast in weather.DailyForecasts) {
+                Forecasts.Add(forecast);
+            }
+        }
+    }
+}
