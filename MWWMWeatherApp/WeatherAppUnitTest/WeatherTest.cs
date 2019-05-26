7728b115139316bbd1e12df300f554327e0239fe
Oskar <kogutoskar123@gmail.com>
2019-05-26 14:25:08 +0200

    Added EmptyQueryTest and ErasingQueryTest

--- MWWMWeatherApp/WeatherAppUnitTest/WeatherTest.cs
+++ MWWMWeatherApp/WeatherAppUnitTest/WeatherTest.cs
@@ -19,9 +19,49 @@ namespace WeatherAppUnitTest {
             //act
             weatherVM.Query = longquery;
 
-            //assety
+            //assert
             int actualQueryLength = weatherVM.Query.Length;
             Assert.AreEqual(actualQueryLength, expectedQueryLength, "Query not cuting strings correctly");
+        }
+
+        /// <summary>
+        /// Tests wheter the application handles an empty query correctly
+        /// </summary>
+        [TestMethod]
+        public void EmptyQueryTest() {
+            //arrange
+            int expectedForecastLength = 0;
+            string query = "";
+            WeatherVM weatherVM = new WeatherVM();
+
+            //act
+            weatherVM.Query = query;
+
+            //assert
+            int actualForecastLength = weatherVM.Forecasts.Count;
+            Assert.AreEqual(actualForecastLength, expectedForecastLength, "Empty query not being handled correctly");
+        }
+
+        /// <summary>
+        /// Checks if the list of forecasts is correctly cleared
+        /// after erasing the query
+        /// </summary>
+        [TestMethod]
+        public void ErasingQueryTest()
+        {
+            //arrange
+            int expectedForecastLength = 0;
+            string firstQuery = "r";
+            string secondQuery = "";
+            WeatherVM weatherVM = new WeatherVM();
+
+            //act
+            weatherVM.Query = firstQuery;
+            weatherVM.Query = secondQuery;
+
+            //assert
+            int actualForecastLength = weatherVM.Forecasts.Count;
+            Assert.AreEqual(actualForecastLength, expectedForecastLength, "Forecast list not erasing correctly");
         }
     }
 }
